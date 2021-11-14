7a033e9a788152371abd67cec5c0f72dbbf3f388
Legendized <93206086+Legendized@users.noreply.github.com>
2021-11-14 21:37:28 +0000

    updated shaders to make fog exponential

--- Sfml gaem/Sfml gaem/Shaders.cs
+++ Sfml gaem/Sfml gaem/Shaders.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using SFML.Graphics;
@@ -9,16 +9,19 @@ namespace SFML.Net_Test
 {
     public static class Shaders
     {
-        public static Color diffuseWithFog(Color MainColor, float normal, float distToCam, float fogThickness)
+        public static Color diffuseWithFog(Color MainColor, float normal, float distToCam, float fogThickness, float fogPower)
         {
             float fog = (distToCam * fogThickness);
-            fog = MathF.Abs(fog);
-            if ((fog) >= 240) fog = 250;
-            float diffuse = MathF.Abs(MathF.Sin(normal/MathF.PI));
+            //fog = MathF.Abs(fog);
+            if ((fog) > 156) fog = 156;
+            float diffuse = MathF.Abs(MathF.Sin(normal / MathF.PI));
             float rfog = fog * (MainColor.R / 255);
             float gfog = fog * (MainColor.G / 255);
             float bfog = fog * (MainColor.B / 255);
-            Color outcol = new Color((byte)(((MainColor.R* diffuse) - rfog)*Program.gamma), (byte)(((MainColor.G * diffuse) - gfog) * Program.gamma), (byte)(((MainColor.B * diffuse) - bfog) * Program.gamma));
+            if (rfog >= 240f) rfog = 240f;
+            if (gfog >= 240f) gfog = 240f;
+            if (bfog >= 240f) bfog = 240f;
+            Color outcol = new Color((byte)(((MainColor.R * diffuse) - MathF.Pow(rfog, fogPower)) * Program.gamma), (byte)(((MainColor.G * diffuse) - MathF.Pow(gfog, fogPower)) * Program.gamma), (byte)(((MainColor.B * diffuse) - MathF.Pow(bfog, fogPower)) * Program.gamma));
             return outcol;
         }
         public static Color FogUnlit(Color MainColor, float distToCam)
